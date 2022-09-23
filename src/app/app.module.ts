d5b960e60739c795f4711bec96af3b2664ff2ef6
Sergio De Luca <sdlbsso@gmail.com>
2022-09-22 23:15:09 -0300

    haciendo menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
+import { AppRouterModule } from './app-router.module';
+
 
 
 
@@ -12,6 +14,7 @@ import { SharedModule } from './shared/shared.module';
   ],
   imports: [
     BrowserModule,
+    AppRouterModule,
     SharedModule
   ],
   providers: [],
