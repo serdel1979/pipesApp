e3f9d8163576d40859b90bddc7dce2e4123c09c6
Sergio De Luca <sdlbsso@gmail.com>
2022-09-23 19:12:49 -0300

    menu rutas y cards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,16 @@
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { AppRouterModule } from './app-router.module';
 import { VentasModule } from './ventas/ventas.module';
 
+import localeEs from '@angular/common/locales/es-AR';
+import localeFr from '@angular/common/locales/fr';
 
-
-
+import { registerLocaleData } from '@angular/common'
+registerLocaleData(localeEs, 'es');
+registerLocaleData(localeFr, 'fr');
 
 @NgModule({
   declarations: [
@@ -19,7 +22,7 @@ import { VentasModule } from './ventas/ventas.module';
     AppRouterModule,
     SharedModule
   ],
-  providers: [],
+  providers: [{ provide: LOCALE_ID, useValue: 'es' }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
